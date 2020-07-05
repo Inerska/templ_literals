438a9d97dd9e9aaddc3cd16e2b2ec04eb3595ea3
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-07-05 23:30:14 +0200

    user-defined literals, function

--- include/templ_literals/main.hpp
+++ include/templ_literals/main.hpp
@@ -1,11 +1,12 @@
 #ifndef TEMPL_LITERALS_MAIN_HPP
 #define TEMPL_LITERALS_MAIN_HPP
 
-namespace N{
-    class Main{
-    public:
+#include <iostream>
+#include <cstring>
+#include <vector>
 
-    };
+namespace templ_lit{
+    std::string operator "" _(const char*, unsigned int);
 }
 
 #endif //TEMPL_LITERALS_MAIN_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include "../include/templ_literals/main.hpp"
 
-using namespace N;
+using namespace templ_lit;
 
-int main(){
-    return 0;
+int main()
+{
+	std::string name{"Adrian"};
+	std::cout << "Hello $.name"_ << std::endl;
+	return 0;
+}
+
+std::string templ_lit::operator "" _(const char *sentence, size_t len)
+{
+	std::string str_sentence{sentence};
+	for (unsigned int i = 0; i < len; ++i)
+	{
+		if (str_sentence[i] == '$' && str_sentence[i + 1] == '.')
+		{
+			
+		}
+	}
+	return "";
 }
