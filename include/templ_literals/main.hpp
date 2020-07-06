4ff67bcaed2643d0f4108c32a66ec72b97219a26
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-07-06 16:24:17 +0200

    Handling numerics

--- include/templ_literals/main.hpp
+++ include/templ_literals/main.hpp
@@ -2,11 +2,61 @@
 #define TEMPL_LITERALS_MAIN_HPP
 
 #include <iostream>
+#include <string>
 
 namespace templ_lit
 {
+	//region PROTOTYPES
 	template<typename ...T>
 	std::string t(T &&...fmt);
+	
+	template<typename T>
+	std::string to_string(const T &arg);
+	
+	std::string to_string(const std::string &arg);
+	
+	std::string to_string(const char *arg);
+	
+	//endregion
+	//region DECLARATIONS
+	template<typename ...T>
+	std::string t(T &&...fmt)
+	{
+		return (... + templ_lit::to_string(fmt));
+	}
+	
+	/**
+	 *
+	 * @tparam const T&
+	 * @return string conversion from const T&
+	 */
+	template<typename T>
+	std::string to_string(const T &arg)
+	{
+		return std::to_string(arg);
+	}
+	
+	/**
+	 *
+	 * @param const std::string&
+	 * @return string conversion from const std::string&
+	 */
+	std::string to_string(const std::string &arg)
+	{
+		return arg;
+	}
+	
+	/**
+	 *
+	 * @param const char*
+	 * @return string conversion from const char*
+	 */
+	std::string to_string(const char *arg)
+	{
+		return arg;
+	}
+	//endregion
 }
+
 
 #endif //TEMPL_LITERALS_MAIN_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,7 @@ using namespace templ_lit;
 int main()
 {
 	std::string name{"Adrian"}, name2{"Alex"};
-	std::cout << templ_lit::t("Hey ", name, " how are u man ?", " wbu ", name2, " ?");
+	int pi{3};
+	std::cout << templ_lit::t("Hey ", name, " how are u man ?", " wbu ", name2, " ?", pi);
 	return 0;
-}
-
-template<typename ...T>
-std::string templ_lit::t(T &&...fmt)
-{
-	return (... + fmt);
 }
